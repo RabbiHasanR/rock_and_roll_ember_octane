cb7a0b8dc3fc250c318408798e232c3fd888ae1d
rabbi50 <jasrabbi50@gmail.com>
2024-03-13 11:14:51 +0600

    end component

---
+++ app/components/star-rating.js
@@ -0,0 +1,19 @@
+import Component from '@glimmer/component';
+
+export default class StarRatingComponent extends Component {
+
+    get maxRating() {
+        return this.args.maxRating ?? 5;
+    }
+
+    get stars () {
+        let stars = [];
+        for (let i = 1; i <= this.maxRating; i++) {
+            stars.push({
+                rating: i,
+                full: i <= this.args.rating,
+            })
+        }
+        return stars;
+    }
+}
