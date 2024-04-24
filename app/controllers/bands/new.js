f8905e74fbde249bbbddb5559d20d0ce1462819c
rabbi50 <jasrabbi50@gmail.com>
2024-04-24 20:28:58 +0600

    advanced routing learning

--- app/controllers/bands/new.js
+++ app/controllers/bands/new.js
@@ -14,6 +14,26 @@ export default class BandsNewController extends Controller {
     return !this.name;
   }
 
+  constructor() {
+    super(...arguments);
+    this.router.on('routeWillChange', (transition) => {
+      console.log('routeWillChange:', transition);
+      if (this.confirmedLeave) {
+        return;
+      }
+      if (transition.from.name === 'bands.new') {
+        if (this.name) {
+          let leave = window.confirm('You have unsaved changes. Are you sure?');
+          if (leave) {
+            this.confirmedLeave = true;
+          } else {
+            transition.abort();
+          }
+        }
+      }
+    });
+  }
+
   @action
   updateName(event) {
     this.name = event.target.value;
@@ -22,6 +42,7 @@ export default class BandsNewController extends Controller {
   @action
   async saveBand() {
     let band = await this.catalog.create('band', { name: this.name });
+    this.confirmedLeave = true;
     this.router.transitionTo('bands.band.songs', band.id);
   }
 }
--- app/routes/bands/new.js
+++ app/routes/bands/new.js
@@ -3,5 +3,6 @@ import Route from '@ember/routing/route';
 export default class BandsNewRoute extends Route {
   resetController(controller) {
     controller.name = '';
+    controller.confirmedLeave = false;
   }
 }
